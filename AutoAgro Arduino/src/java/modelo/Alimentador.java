dd1bdaa1adc8a5821e0de5693dbf07a3c148c7e0
CarloxCss <64516341+CarloxCss@users.noreply.github.com>
2020-09-08 12:50:45 -0300

    Add files via upload

---
+++ AutoAgro Arduino/src/java/modelo/Alimentador.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Alimentador {
+    
+    private int idAlimentador;
+    private String numeroIdentificacaoAlimentador;
+    private String descricaoAlimentador;
+    
+    private Usuario usuario;
+    
+
+    public String getDescricaoAlimentador() {
+        return descricaoAlimentador;
+    }
+
+    public void setDescricaoAlimentador(String descricaoAlimentador) {
+        this.descricaoAlimentador = descricaoAlimentador;
+    }
+    
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getIdAlimentador() {
+        return idAlimentador;
+    }
+
+    public void setIdAlimentador(int idAlimentador) {
+        this.idAlimentador = idAlimentador;
+    }
+
+    public String getNumeroIdentificacaoAlimentador() {
+        return numeroIdentificacaoAlimentador;
+    }
+
+    public void setNumeroIdentificacaoAlimentador(String numeroIdentificacaoAlimentador) {
+        this.numeroIdentificacaoAlimentador = numeroIdentificacaoAlimentador;
+    }
+    
+    
+    
+}
